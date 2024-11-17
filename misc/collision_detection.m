6d927fe49f96620ed894f3b5187fcee98ca48904
Cagin Agirdemir <caginagirdemir@gmail.com>
2024-11-17 10:43:10 +0100

    Update and rename collision_detection.m to misc/collision_detection.m

--- misc/collision_detection.m
+++ misc/collision_detection.m
@@ -1,10 +1,10 @@
 clear all
 clc
-m1=1; %kütle
+m1=1; %kÃ¼tle
 r1=1; %yaricap
 X1=[3,3]; %ilk pozisyon
 V1=[0.1,0.0]; %ilk hiz
-m2=2; %kütle
+m2=2; %kÃ¼tle
 r2=1; %yaricap
 X2=[10,4]; %ilk pozisyon
 V2=[-0.1,0]; %ilk hiz
@@ -24,7 +24,7 @@ for i=1:5000
     X1=X1+V1; %konuma hiz eklenerek yeni konum elde edilmesi
     X2=X2+V2;   %konuma hiz eklenerek yeni konum elde edilmesi
     
-    %top 1 in border çarp??ma kontrolü
+    %top 1 in border Ã§arp??ma kontrolÃ¼
     if (X1(1)<=r1)
         X1(1)=r1;
         V1(1)=-V1(1);
@@ -42,7 +42,7 @@ for i=1:5000
         V1(2)=-V1(2);
     end
     
-    %top 2 in border çarp??ma kontrolü
+    %top 2 in border Ã§arp??ma kontrolÃ¼
     if (X2(1)<=r2)
         X2(1)=r2;
         V2(1)=-V2(1);
@@ -60,12 +60,12 @@ for i=1:5000
         V2(2)=-V2(2);
     end
     
-    if (norm (X1-X2)<=r1+r2) %carpisma kontrolü
+    if (norm (X1-X2)<=r1+r2) %carpisma kontrolÃ¼
     
-    u1=V1; %carpisma öncesi hiz u1 de?iskenine
-    u2=V2;  %carpisma öncesi hiz u2 de?iskenine
+    u1=V1; %carpisma Ã¶ncesi hiz u1 de?iskenine
+    u2=V2;  %carpisma Ã¶ncesi hiz u2 de?iskenine
     
-    %vektör normallerini hesaplama
+    %vektÃ¶r normallerini hesaplama
     u12=norm(u1)*cos(atan2(u1(2),u1(1))-atan2(X2(2)-X1(2),X2(1)-X1(1)));
     u11=norm(u1)*sin(atan2(u1(2),u1(1))-atan2(X2(2)-X1(2),X2(1)-X1(1)));
     u21=norm(u2)*cos(atan2(u2(2),u2(1))-atan2(X1(2)-X2(2) , X1(1)-X2(1)));
@@ -73,15 +73,15 @@ for i=1:5000
     v12 = ( (m1-m2)*u12 - 2*m2*u21 ) / (m1+m2); 
     v21 = ( (m1-m2)*u21 + 2*m1*u12 ) / (m1+m2);
     
-    %capisma sonrasi hiz vektörü hesaplama
+    %capisma sonrasi hiz vektÃ¶rÃ¼ hesaplama
     V1=u11*[-sin(atan2(X2(2)-X1(2) , X2(1)-X1(1))),cos(atan2(X2(2)-X1(2) , X2(1)-X1(1)))] + v12*[cos(atan2(X2(2)-X1(2) , X2(1)-X1(1))),sin(atan2(X2(2)-X1(2) , X2(1)-X1(1)))];
     V2=u22*[-sin(atan2(X1(2)-X2(2) , X1(1)-X2(1) )),cos(atan2(X1(2)-X2(2) , X1(1)-X2(1) ))] - v21*[cos(atan2(X1(2)-X2(2) , X1(1)-X2(1) )),sin(atan2(X1(2)-X2(2) , X1(1)-X2(1) ))];
     
-end %if, carpisma kontrolü
+end %if, carpisma kontrolÃ¼
 
 %ekrana plot ettirme
     set(ball1,'XData',X1(1)+r1*Xcircle, 'YData', X1(2)+r1*Ycircle);
     set(ball2,'XData',X2(1)+r2*Xcircle, 'YData', X2(2)+r2*Ycircle);
     drawnow;
 
-end %for döngüsü
+end %for dÃ¶ngÃ¼sÃ¼
